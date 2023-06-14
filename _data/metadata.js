c75d62fc82f8272e874745553f48d9c68dbf07a9
chanditha2023 <chandithaweb@gmail.com>
2023-06-14 18:22:59 +0100

    Initial Changes

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Ernest Wickrema",
+		email: "chandithaint@gmail.com",
+		url: "https://main--ubiquitous-kangaroo-5113f9.netlify.app/"
 	}
 }
---
+++ content/blog/mypost.md
@@ -0,0 +1,6 @@
+---
+title: This is my post 
+date: 2023-06-14
+draft: true
+---
+This is created as a study material for the TCG web development course 
